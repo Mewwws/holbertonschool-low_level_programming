6fa5ade1c9af39a88529112dfa8b871043a6aa8c
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-10 13:12:01 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - prints a message
+ * returns: int 0 success
+ */
+int main(void)
+{
+	/** prints + newline */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
