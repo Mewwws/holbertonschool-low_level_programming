edfd1fcf672cffc9a91d584b9512ba8063cb9f2f
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-11 12:29:41 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - prints 0 - 9
+ * Return: result
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
