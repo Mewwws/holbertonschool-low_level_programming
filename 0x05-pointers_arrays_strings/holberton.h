2502f96f9dc4bd39b815aca8bd64b6ea51a84177
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-16 14:57:22 +0100

    end of school day

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,21 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 #include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-
-}
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -28,4 +12,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
