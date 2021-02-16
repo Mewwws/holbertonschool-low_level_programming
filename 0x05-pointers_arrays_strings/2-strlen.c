66199b70f972cd0d74b0cbbb312864a0b6593fd6
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-16 10:45:40 +0100

    string length calculator

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
 
+/**
+ * _strlen - calculates string length
+ * @s: string
+ * Return: length
+ */
 int _strlen(char *s)
 {
 	int i;
-	i = 0;
 
-	while(s[i] != '\0')
+	i = 0;
+	while (s[i] != '\0')
 	{
 		i++;
 	}
