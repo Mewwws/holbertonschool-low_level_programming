4e7135c317653417ac2b794c21ffce2de8dbbda7
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-16 14:02:05 +0100

    prints an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array
+ * @a: array
+ * @n: array's number
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
