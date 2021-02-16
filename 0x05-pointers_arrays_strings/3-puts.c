05b247d208826439ad01d35ad2e56b75e26a25c4
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-16 10:49:02 +0100

    string printer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * _puts - prints a string
- *
+ * @str: string
  * Return: 0
  */
 
@@ -11,10 +11,10 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != \0)
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
