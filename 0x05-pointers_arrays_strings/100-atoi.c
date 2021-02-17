7b7efe0726f811e962d43bbe4a2ee5cf44c1889f
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-17 13:12:43 +0100

    unfinished atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: converted string
+ * Return: inted string
+ */
+int _atoi(char *s)
+{
+	int c;
+	int i;
+	int j;
+	int o;
+
+	c = 0;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	char m[c];
+
+	j = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ('0' <= s[i] && s[i] <= '9') && (!('0' <= s[i + 1]) && !(s [i + 1] <= '9'))
+						   {
+							   m[j] = 
+						   }
+
+	}
+
+
+
+	o = 0;
+
+
+	return (o);
+
+}
