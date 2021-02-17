2742f81fa4b7d352930ac0dacc0ce00fa8c743f8
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-17 11:53:04 +0100

    trying

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,10 @@ void puts2(char *str)
 {
 	int i;
 
+	s[0] = '\0'
 	for (i = 0; str[i + 1] != '\0' && str[i] != '\0'; i = i + 2)
 	{
-		if (str[i] != '\0')
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i + 1]);
 	}
 	_putchar('\n');
 }
