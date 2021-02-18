cc117008ffba0fb2caf974e8ea528a70da9e7e76
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-18 14:29:20 +0100

    betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 
 /**
- * *_string_toupper - convers string lowercase to caps
- *
+ * *string_toupper - convers string lowercase to caps
+ * @s: converted string
+ * Return: s
  */
 
- char *string_toupper(char *s)
- {
-	 int i;
+char *string_toupper(char *s)
+{
+	int i;
 
-	 for (i = 0; s[i] != '\0'; i++)
-	 {
-		 if (s[i] >= 97 && s[i] <= 122)
-		 {
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
 			s[i] = s[i] - 32;
-		 }
-	 }
-	 return (s);
- }
+		}
+	}
+	return (s);
+}
