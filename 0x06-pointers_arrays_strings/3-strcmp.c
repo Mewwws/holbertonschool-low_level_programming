26bb15b2701c559f4fffafa84f5ae3e92e1e7332
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-18 17:25:47 +0100

    attempt

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		j = j + s1[i] - s2[i];
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
 	}
-	return (j);
+	return (s1[i] - s2[i]);
 }
