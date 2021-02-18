6e81398c547f1cb5832e7b20374d120c3b973512
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-18 20:30:18 +0100

    number printer

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -7,22 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned char p;
+	unsigned int p;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n > 10)
+	p = n;
+	if ((p / 10) > 0)
 	{
-		print_number(n / 10);
-		p = n;
-		_putchar(p + '0');
+		print_number(p / 10);
 	}
-	else
-	{
-		p = (n % 10);
-		_putchar(p + '0');
-	}
+	_putchar((p % 10) + '0');
 }
