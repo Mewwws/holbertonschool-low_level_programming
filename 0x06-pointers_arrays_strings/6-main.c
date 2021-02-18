48916ca12710237529e3d18b1ba4ff50979c9ccd
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-18 12:13:45 +0100

    bonus files

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n; a\ta\na,a;a.a?a!a\"a(a)a{a}a";
     char *p;
 
     p = cap_string(s);
