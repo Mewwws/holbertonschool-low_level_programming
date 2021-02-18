609fc0adf7b406d84e84bf72fe83b60db915a16d
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-18 14:36:40 +0100

    compare fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int j = 0;
-	int b = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s2[i] == '\0')
-		{
-			b = 1;
-		}
 		j = j + s1[i] - s2[i];
-	}
-	if (b == 0)
-	{
-		for ( ; s2[i] != '\0'; i++)
-		{
-			j = j + s1[i] - s2[i];
-		}
 	}
 	return (j);
 }
