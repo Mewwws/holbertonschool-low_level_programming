e304a96b2f04817c00c700fb6a8283d665f4e0f2
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-19 12:58:04 +0100

    print number?

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,24 @@
+ 
+#include "holberton.h"
+
+/**
+ * print_number - prints number using only _putchar
+ * @n: number to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int p;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	p = n;
+	if ((p / 10) > 0)
+	{
+		print_number(p / 10);
+	}
+	_putchar((p % 10) + '0');
+}
