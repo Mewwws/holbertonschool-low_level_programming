1e7861e7cbbc29af058d95c017593201fc243a51
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-19 11:51:24 +0100

    fizz bizz stuff

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * main - prints numbers from 1 to 100
+ * while replacing 3 and 5 multiples
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d",i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
