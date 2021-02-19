30307328d5df941e32b265847be6472d119ac8b5
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-19 11:01:12 +0100

    uppercase checker

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+/**
+ * _isupper - checks for uppercase
+ * @c: checked variable
+ * Return: 1 if upper; else 0
+ */
 int  _isupper(int c)
 {
-	int c;
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 
-	isupper(int c);
+
 }
