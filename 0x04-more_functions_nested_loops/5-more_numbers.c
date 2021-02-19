5fcf641562ef75352a4178f47fe0fa21e2f08d44
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-19 10:47:53 +0100

    prints 0-14 *10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,18 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
-	for (i = 0 ; i < 15; i++)
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0 ; i < 15; i++)
 	{
 		if (i / 10 > 0)
 		{
 			_putchar('1');
 		}
-		_putchar(i % 10);
-		_putchar('\n');
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
 	}
 }
