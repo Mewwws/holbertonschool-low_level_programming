01f1524c78eff0996730f21ebd377d41d7ef06ea
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-22 12:41:44 +0100

    scans for full string in another one

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - searches for first occurrence of needle in haystack
+ * @needle: searches that
+ * @haystack: search it here
+ * Return: pointer to result
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack != '\0')
+	{
+		haystack++;
+		if (*haystack == needle[0])
+		{
+			for (i = 0; needle[i] != '\0'; i++)
+			{
+				if (haystack[i] == needle[i])
+				{
+					continue;
+				}
+				else
+				{
+					break;
+				}
+			}
+			if (needle[i] == '\0')
+			{
+				break;
+			}
+		}
+	}
+	return (haystack);
+}
