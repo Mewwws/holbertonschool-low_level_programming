f55856a969c8ab918a023d9f3571d8b7979e8d2a
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-23 09:04:05 +0100

    NULL

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,19 +10,22 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	while (*haystack != '\0')
+	if (needle[0] != '\0')
 	{
-		haystack++;
-		if (*haystack == needle[0])
+		while (*haystack != '\0')
 		{
-			i = 0;
-			while (needle[i] != '\0' && haystack[i] != '\0' && haystack[i] == needle[i])
+			haystack++;
+			if (*haystack == needle[0])
 			{
-				i++;
-			}
-			if (needle[i] == '\0')
-			{
-				return (haystack);
+				i = 0;
+				while (needle[i] != '\0' && haystack[i] != '\0' && haystack[i] == needle[i])
+				{
+					i++;
+				}
+				if (needle[i] == '\0')
+				{
+					return (haystack);
+				}
 			}
 		}
 	}
