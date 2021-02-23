4d3bbce5b76b9b3d2bcc2716b02a9ed80758a42c
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-23 12:49:51 +0100

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,23 +13,29 @@ void times_table(void)
 	{
 		for (i = 0; i < 10; i++)
 		{
-			_putchar('0');
+			if (i == 0)
+			{
+				_putchar('0');
+			}
 			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			if ((j * i) % 100 > 0)
+			{
+				_putchar (j * i / 100 + '0');
+			}
+			else
+			{
+				_putchar (' ');
+			}
 			if (j * i / 10 == 0)
 			{
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar(j * i / 10 + '0');
+				_putchar(((j * i) % 100) / 10 + '0');
 			}
 			_putchar((j * i) % 10 + '0');
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
