447ba683e7c20251b109be82373cfaa39835a8c5
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-23 11:56:42 +0100

    hjkb

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+* add - adds
+* Return: reseult
+*/
+int add(int a, int b)
+{
+	a = a + b;
+	return (a);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,25 +9,27 @@ void times_table(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		j = i * 9;
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(i + '0');
-		_putchar(',');
-		_putchar(' ');
-		if (j / 10 == 0)
+		for (i = 0; i < 10; i++)
 		{
+			_putchar('0');
+			_putchar(',');
 			_putchar(' ');
+			_putchar(' ');
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+			if (j * i / 10 == 0)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(j * i / 10 + '0');
+			}
+			_putchar((j * i) % 10 + '0');
+			_putchar('\n');
 		}
-		else
-		{
-			_putchar(j / 10 + '0');
-		}
-		_putchar(j % 10 + '0');
-		_putchar('\n');
 	}
 }
