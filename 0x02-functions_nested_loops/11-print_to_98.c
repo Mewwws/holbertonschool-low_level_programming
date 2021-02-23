2409d39d095a08f2ae58b333cc026567bba14f0a
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-23 12:13:23 +0100

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,16 @@
 */
 void print_to_98(int n)
 {
-	for (; n < 98; n++)
+	for (; n <= 98; n++)
 	{
 		printf("%d", n);
+		if (n < 98)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
 }
