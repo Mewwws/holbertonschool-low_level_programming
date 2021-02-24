e85de5c6d570bec406c1119f842008c2e00a990f
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-24 11:15:17 +0100

    without recursion :/ (palindrome)

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* is_palindrome - checks if palindrome
+* Return: 1 if yes, 0 if no
+*/
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	for (j = 0; j < i; j++)
+	{
+		if (j > i /2)
+		{
+			return (1);
+		}
+		if (s[j] != s[i - j])
+		{
+			break;
+		}
+	}
+	return (0);
+}
