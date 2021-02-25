4c3637e270c92ac8ddbfe5c4820455589f3018aa
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-25 13:40:28 +0100

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	for (az = 'A'; az <= 'Z'; az++)
+	{
+		putchar(az);
+	}
+	putchar ('\n');
+	return (0);
+}
