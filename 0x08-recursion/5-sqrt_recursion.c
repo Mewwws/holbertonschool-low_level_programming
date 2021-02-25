0a2ed469b507fc5470b7e97ce2652bbdc959e64e
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-25 10:36:22 +0100

    idk

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,26 @@
 #include "holberton.h"
 
 /**
-* _sqrt_recursion - square root with recursion?
+* manual - actually does all the job
 * @n: search square root of this
+* @i: variable square attempt
 * Return: root or -1
 */
+int manual(int n, int i)
+{
+	if (i * i < n)
+		return (manual(n, i + 1));
+	if (i * i == n)
+		return (i);
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - square root with recursion?
+ * @n: search square root of this
+ * Return: manual's
+ */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n < 0)
-	{
-		return (-1);
-	}
-	for (i = 0; i <= n; i++)
-	{
-		if (i * i == n)
-		{
-			return (i);
-		}
-		if (i * i > n)
-		{
-			return (-1);
-		}
-	}
-	return (-1);
+	return (manual(n, 0));
 }
