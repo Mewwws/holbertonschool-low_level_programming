cd4f22f2ef0add21643e1ec6cd22b807788f74e3
Mehdi <mehdi.boulifa@yahoo.fr>
2021-02-25 10:51:04 +0100

    maybe

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,27 @@
 #include "holberton.h"
 
 /**
-* is_prime_number
-* Return:
+* is_prime_number - checks for prime numbers
+* @n: checked number
+* Return: primanual
 */
 int is_prime_number(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (primanual(n, 2));
+}
+/**
+ * primanual - does the actual job
+ * @n: checked number
+ * @i: checker
+ * Return: 1 if prime, 0 if composed
+ */
+int primanual(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (i * 2 > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (primanual(n, i + 1));
 }
