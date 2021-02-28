285afde52e978c1490cef4b9fd6b9cf051de669b
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-28 12:04:40 +0100

    not working

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,15 +10,32 @@ int _atoi(char *s)
 	int i;
 	int m = 0;
 	int o = 1;
+	int n = 0;
 
+	if (*s == '\0')
+		return (0);
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ('0' <= s[i] && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			m = m * 10 + s[i] - '0';
+			for (i = 0; s[i] != '\0'; i++)
+			{
+				if (s[i] >= '0' && s[i] <= '9')
+				{
+					m = m * 10 + s[i] - '0';
+					n = 1;
+				}
+				else
+				{
+					return (o * m);
+					break;
+				}
+			}
 		}
+		if (n == 1)
+			break;
 		if (s[i] == '-')
-			o == -1;
+			o = o * -1;
 	}
-	return (m * o);
+	return (o * m);
 }
