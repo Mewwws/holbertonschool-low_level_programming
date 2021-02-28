c23776344870a5b01186c8acaf0f86dc3b91f0f2
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-28 12:50:15 +0100

    alot of stuff

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates strings
+ * @src: is concatenated
+ * @dest: gets concatenated into
+ * Return: *dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
