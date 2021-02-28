59982c6208ee1663362b937b5556965e62e7c866
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-28 14:48:02 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,14 +12,16 @@ int main(int argc, char *argv[])
 {
 	unsigned int i = 0;
 	int j;
+	int k;
 
 	for (j = 1; j < argc; j++)
 	{
-		if (*argv[j] < '0' || *argv[j] > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (k = 0; argv[j][k] != '\0'; k++)
+			if (argv[j][k] < '0' || argv[j][k] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		i += atoi(argv[j]);
 	}
 	printf("%d\n", i);
