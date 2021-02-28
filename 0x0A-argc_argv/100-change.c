cadec06d3028e81c3b0754d4cd7d447a9120fb6f
Mewws <mehdi.boulifa@yahoo.fr>
2021-02-28 17:02:40 +0100

    m

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,16 +10,16 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	int i = 0;
 	int j = atoi(argv[1]);
 	int k[5] = {25, 10, 5, 2, 1};
 	int l;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	for (l = 0; l < 5; l++)
 	{
 	if (j >= k[l])
