4b05789a397f0266b5d292bfbc812bc24a2ff6d1
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-03 12:22:19 +0100

    cat technique

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,25 +15,29 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j;
 
 	i = 0;
-	for (j = 0; s1[j] != '\0'; j++)
-		i++;
-	for (j = 0; s2[j] != '\0'; j++)
-		i++;
+	if (s1 != 0)
+		for (j = 0; s1[j] != '\0'; j++)
+			i++;
+	if (s2 != 0)
+		for (j = 0; s2[j] != '\0'; j++)
+			i++;
 	i++;
 	s = malloc(i * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	i = 0;
-	for (j = 0; s1[j] != '\0'; j++)
-	{
-		s[i] = s1[j];
-		i++;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		s[i] = s2[j];
-		i++;
-	}
+	if (s1 != 0)
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			s[i] = s1[j];
+			i++;
+		}
+	if (s2 != 0)
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			s[i] = s2[j];
+			i++;
+		}
 	i++;
 	s[i] = '\0';
 	return (s);
