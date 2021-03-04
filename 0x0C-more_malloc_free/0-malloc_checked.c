9ea2a7ff36777eb82fa35f6db6910b47747351ec
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-04 20:13:55 +0100

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - alloc with malloc and chec if it worc
+ * @b: size
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
