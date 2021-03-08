1dd93d97433093ecdabc8f7ef74cd92a70738dbc
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-08 23:04:57 +0100

    start

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,8 +5,14 @@
  * @name: doge name
  * @age: doge age
  * @owner: doge owner
+ * Return: anime doge
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	return (0);
+	dog_t *korone;
+
+	korone = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	return (korone);
 }
