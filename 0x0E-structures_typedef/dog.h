16dbd6d0236689cc1585e10516ddeeee706977f9
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-08 21:14:52 +0100

    start

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef _DOGE
 #define _DOGE
+/**
+ * struct dog - doge id
+ * @name: doge name
+ * @age: doge float
+ * @owner: doge owner
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
