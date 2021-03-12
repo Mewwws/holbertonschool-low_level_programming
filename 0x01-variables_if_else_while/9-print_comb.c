6d05683a831cb53e65661b6403364ff95dfe104d
Mehdi <mehdi.boulifa@yahoo.fr>
2021-03-12 10:33:02 +0100

    old

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints 0 - 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+	}
+	putchar ('\n');
+	return (0);
+}
