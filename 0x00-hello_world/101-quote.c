cbec390f3a9ed8e3dcc2d300bf6b32037fc5baa4
Mehdi <mehdi.boulifa@yahoo.fr>
2021-03-12 10:12:59 +0100

    writer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints string
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
 }
