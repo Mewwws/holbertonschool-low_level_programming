2b2ccc78929550a3d344d90215f552d696ba9e53
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 13:09:08 +0100

    io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int a, c;
+	int a, c, b;
 
 	if (filename == NULL)
 		return (-1);
 	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (a < 0)
 		return (-1);
-	c = write(a, text_content, sizeof(text_content));
+	for (b = 0; text_content[b] != '\0'; b++)
+		continue;
+	c = write(a, text_content, b);
 	if (c < 0)
 		return (-1);
 	return (1);
