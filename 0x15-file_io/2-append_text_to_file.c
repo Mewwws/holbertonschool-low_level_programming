6688760c43fc7282e376bdcc2cc4e6c0d4955963
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 13:50:08 +0100

    io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	a = open(filename, O_WRONLY);
+	a = open(filename, O_WRONLY | O_APPEND);
 	if (a == -1)
 		return (-1);
 	c = write(a, text_content, b);
