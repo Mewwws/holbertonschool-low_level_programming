8c71e5ff2f85fba98a81edb5a4149a2768d1ec0c
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 12:22:33 +0100

    IO

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the created file
+ * @text_content: string to put in the file
+ * Return: 1 if success / -1 if failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a, c;
+	char *b;
+
+	if (filename == NULL)
+		return (-1);
+	a = open("filename", O_CREAT | O_WRONLY, 0600);
+	if (a < 0)
+		return (-1);
+	c = write(a, text_content, sizeof(*text_content));
+	if (c < 0)
+		return (-1);
+	return (1);
+}
