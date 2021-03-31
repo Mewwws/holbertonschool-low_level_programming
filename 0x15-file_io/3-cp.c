d8a1116702d6d81cbc27bb623f69a2e4ddac0bf1
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 16:16:03 +0100

    io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,21 +13,24 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	/* opening */
 	a = open(av[1], O_RDONLY);
 	if (a == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	b = open(av[2], O_CREAT | O_RDONLY | O_TRUNC, 0664);
+	b = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (b == -1)
 	{
 		close(a);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+	/* copying */
 	while ((c = read(a, e, 1024)) > 0)
 	{
+		printf("%s\n", e);
 		d = write(b, e, c);
 		if (d == -1)
 		{
@@ -40,6 +43,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+	/* closing */
 	c = close(a);
 	if (c == -1)
 	{
