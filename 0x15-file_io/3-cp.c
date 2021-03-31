5a9f7f65390720ac78c6969bf54aa9353dd3d51d
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 16:37:44 +0100

    io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
-/** closing - closes files from their fd
+
+/**
+ * closing - closes files from their fd
  * @fd: file code
  * @c: checker
  * Return: void
@@ -12,11 +14,12 @@ void closing(int fd, int c)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
 		exit(100);
 	}
-	
 }
 
 /**
  * main - copies content from file to another
+ * @ac: arguments count
+ * @av: arguments string pointer
  * Return: 1 if success / 97 -> 100 if error
  */
 int main(int ac, char **av)
@@ -29,7 +32,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	/* opening */
 	a = open(av[1], O_RDONLY);
 	if (a == -1)
 	{
@@ -43,7 +45,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	/* copying */
 	while ((c = read(a, e, 1024)) > 0)
 	{
 		d = write(b, e, c);
@@ -58,7 +59,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	/* closing */
 	closing(a, c);
 	closing(b, c);
 	return (0);
