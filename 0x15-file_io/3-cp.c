9e78a6ae1334b5c19a0e3fce0694a158921904ca
Mewws <mehdi.boulifa@yahoo.fr>
2021-03-31 15:46:31 +0100

    io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,10 @@ int main(int ac, char **av)
 	}
 	a = open(av[1], O_RDONLY);
 	if (a == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(99);
+	}
 	b = open(av[2], O_CREAT | O_RDONLY | O_TRUNC, 0664);
 	if (b == -1)
 	{
