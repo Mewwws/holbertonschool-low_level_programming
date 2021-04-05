8c335e43f7c83aeff032db3cbfb7af6ed1be1b9b
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-05 12:51:18 +0100

    working

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,28 +13,16 @@ void times_table(void)
 	{
 		for (i = 0; i < 10; i++)
 		{
-			if (i == 0)
-			{
-				_putchar('0');
-			}
-			_putchar(',');
-			if ((j * i) % 100 > 0)
-			{
+			if ((j * i) / 100 > 0)
 				_putchar (j * i / 100 + '0');
-			}
 			else
-			{
 				_putchar (' ');
-			}
-			if (j * i / 10 == 0)
-			{
+			if ((j * i) < 10)
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar(((j * i) % 100) / 10 + '0');
-			}
 			_putchar((j * i) % 10 + '0');
+			_putchar(',');
 		}
 		_putchar('\n');
 	}
