84549acd73cd0e1333cecfcf24832509e2d59758
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-06 14:38:18 +0100

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - gets a specific bit from a number
  * @n: number to take bit from
- * @index: index of the n to return 
+ * @index: index of the n to return
  * Return: bit value (1 or 0)
  */
 int get_bit(unsigned long int n, unsigned int index)
