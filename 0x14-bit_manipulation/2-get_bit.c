c96b813868047a4674fa4c7dbca5ba66692fad76
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-06 14:07:47 +0100

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > sizeof(n) * 8 - 1)
+		return (-1);
 	i = (n >> index) & 1;
 	if (i != 0 && i != 1)
 		return (-1);
