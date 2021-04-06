7bd3466e234d3d154e0ed6aa2185bbeda0caa96d
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-06 16:16:19 +0100

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *s = malloc(size * sizeof(char));
+	char *s;
 
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
