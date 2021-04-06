ffdd97bb326564bbe3ee826f19abeb19dd9dcf7e
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-06 16:32:37 +0100

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,17 +18,17 @@ int **alloc_grid(int width, int height)
 	ar = malloc(sizeof(int *) * height);
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0, i < height, i++)
+	for (i = 0; i < height; i++)
 	{
 		ar[i] = malloc(sizeof(int) * width);
 		if (ar[i] == NULL)
 		{
-			for (, i >= 0, i--)
+			for (; i >= 0; i--)
 				free(ar[i]);
 			free(ar);
 			return (NULL);
 		}
-		for (j = 0; j < width, j++)
+		for (j = 0; j < width; j++)
 			ar[i][j] = 0;
 	}
 	return (ar);
