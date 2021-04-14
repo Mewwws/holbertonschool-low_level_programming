a42863a47baeff619aed0456a6fb624fb813fe01
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-14 21:58:48 +0100

    not working

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -9,54 +9,76 @@
 char **strtow(char *str)
 {
 	char **a;
-	int i, j, k, words = 0, lets, letters = 0;
+	int i, word = 0, k, o, words = 0, len = 0, letters = 0;
+	char *b;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
+	words = words_count(str);
+	if (words == 0)
+		return (NULL);
+	a = malloc(sizeof(char *) * (words + 1));
+	if (a == NULL)
+		return (NULL);
+	while (str[i] != '\0' && word < words)
+	{
+		if (str[i] == ' ')
+			i++;
+		else
+		{
+			b = str + i;
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				len++;
+				i++;
+			}
+			len++;
+			a[word] = malloc(sizeof(char) * len);
+			if (a[word] == NULL)
+			{
+				while (word >= 0)
+				{
+					free(a[word]);
+					word--;
+				}
+				free (a);
+				return (NULL);
+			}
+			o = 0;
+			while (*b != '\0' && *b != ' ')
+			{
+				a[word][o] = *b;
+				o++;
+				b++;
+			}
+			a[word][o] = '\0';
+			word++;
+			len = 0;
+			i++;
+		}
+	}
+	return (a);
+}
+/**
+ * words_count - counts words
+ * @str: counter string
+ * Return: number of words
+ */
+int words_count(char *str)
+{
+	int i;
+	int words = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ')
 			i++;
 		else
 		{
-			for (; str[i] != ' ' && str[i] != '\0'; i++)
-				letters++;
+			while (str[i] != ' ' && str[i] != '\0')
+			       i++;
 			words++;
 		}
 	}
-	a = malloc(sizeof(char *) * (words + 1));
-	if (a == NULL)
-		return (NULL);
-	j = 0;
-	while (str[i] != '\0')
-	{
-		lets = 0;
-		if (str[i] == ' ')
-			i++;
-		else
-		{
-			while (str[i] != ' ' && str[i] != '\0')
-			{
-				lets++;
-			}
-			lets++;
-			a[j] = malloc(sizeof(char) * lets);
-			if (a[j] != NULL)
-			{
-				for (i = 0; i < j; i++)
-					free(a[i]);
-				free (a);
-				return (NULL);
-			}
-			i = (i + 1) - lets;
-			for (k = 0; str[i] != '\0' && str[i] != ' '; k++)
-			{
-				a[j][k] = str[i];
-			}
-			a[j][k] = '\0';
-				j++;
-		}
-	}
-	a[j] = NULL;
-	return (a);
+	return (words);
 }
