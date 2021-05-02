46cfc554722ca2ca11caa5740e61a328f6c86b31
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 17:41:06 +0100

    cycle

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - goes to indexth node
+ * @head: dlist pointer
+ * @index: indexth node number
+ * Return: pointer to new node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *n;
+
+	while (index)
+	{
+		index--;
+		head = head->next;
+		n = head;
+	}
+	return (n);
+}
