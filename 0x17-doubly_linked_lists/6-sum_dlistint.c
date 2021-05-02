83fecc8c3e016882dadce7eab554280d0b20b1fc
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 17:50:58 +0100

    cycle

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - counts sum of doublinst
+ * @head: dlist pointer
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int i = head->n;
+
+	if (head == NULL)
+		return (0);
+	while (head->next)
+	{
+		head = head->next;
+		i = head->n;
+	}
+	return (i);
+}
