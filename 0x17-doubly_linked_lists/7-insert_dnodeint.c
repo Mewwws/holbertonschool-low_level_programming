82469c8ec1edf01d649539540ac93c883bc9d6f0
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 18:26:16 +0100

    cycle

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,13 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (idx && (*h)->next)
+	while (idx)
 	{
 		idx--;
 		*h = (*h)->next;
+		if (!(*h))
+			return (NULL);
+
 	}
-	if (idx)
-		return (NULL);
 	if ((*h)->next == NULL)
 		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
