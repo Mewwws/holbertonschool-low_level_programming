3b36b18295e8de8bc4c156f56811bb1e43c2379f
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 11:44:45 +0100

    boxes

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - dlisted add node at beginning
+ * @head: header node pointer
+ * Return: number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		*head->prev = new;
+	return (new);
+	
+}
