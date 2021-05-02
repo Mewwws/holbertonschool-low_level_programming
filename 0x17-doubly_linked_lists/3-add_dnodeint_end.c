d4079ddb30ba9c9a1c6650123b964c8c71b346af
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 12:00:52 +0100

    boxes

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - dlisted add node at the end
+ * @head: header node pointer
+ * @n: value to insert
+ * Return: pointer to new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last = *head;
+
+	if (head == NULL)
+		return (NULL);
+	while ((*last)->next)
+		*last = (*last)->next;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = *last;
+	if (*last != NULL)
+		(*last)->next = new;
+	return (new);
+}
