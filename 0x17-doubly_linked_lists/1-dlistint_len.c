bdc1b707363326a23dc7090f2dbf7e1732002cc1
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 11:35:15 +0100

    boxes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,14 +5,16 @@
  * @h: header node pointer
  * Return: number of nodes
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h)
 	{
-		n++;
 		h = h->next;
+		n++;
 	}
 	return (n);
 }
