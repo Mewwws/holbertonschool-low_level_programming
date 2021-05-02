df3f249e7ea35ba354704d3e65e02e5849753b45
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-02 11:24:29 +0100

    boxes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - dlisted printer
+ * dlistint_len - dlisted counter
  * @h: header node pointer
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h);
 {
 	size_t n = 0;
 
