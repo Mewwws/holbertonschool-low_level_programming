fceb85e82a100565ff8a8dc8560bf8d548c32290
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-03 23:59:57 +0100

    m

---
+++ 0x18-dynamic_libraries/calcul.c
@@ -0,0 +1,60 @@
+#include "calcul.h"
+
+/**
+* add - adds
+* @a: added
+* @b: added
+* Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+#include "holberton.h"
+
+/**
+* sub - sub
+* @a: subed
+* @b: subed
+* Return: a + b
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+#include "holberton.h"
+
+/**
+* mul - mul
+* @a: muled
+* @b: muled
+* Return: a + b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+#include "holberton.h"
+
+/**
+* div - div
+* @a: dived
+* @b: dived
+* Return: a + b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+#include "holberton.h"
+
+/**
+* mod - mod
+* @a: modded
+* @b: modded
+* Return: a + b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
